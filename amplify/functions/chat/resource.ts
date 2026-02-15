95addbd61df21b3bc490d5144fb877eb5bf1a1a9
benjoldham <benjoldham17@gmail.com>
2026-02-15 16:41:20 +0000

    Update resource.ts

--- amplify/functions/chat/resource.ts
+++ amplify/functions/chat/resource.ts
@@ -6,6 +6,7 @@ export const chatFunction = defineFunction({
   timeoutSeconds: 60,
   environment: {
     DEFAULT_MODEL_KEY: 'claude_haiku',
+    DEFAULT_MAX_OUTPUT_TOKENS: '2500',
   },
   permissions: [
     {
